c6b38ea90f919725ab4a78f43fe55cd8acd20461
unknown <cuzawzawmyint@gmail.com>
2025-11-08 00:08:32 +0700

    fix refetch in home paged dynamic in prod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import prisma from "@/lib/prisma";
 import UrlList from "../components/url/UrlList";
 import { wait } from "@/lib/helper";
 
+// Make this page dynamic
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
   // fetch data from db
   await wait();
